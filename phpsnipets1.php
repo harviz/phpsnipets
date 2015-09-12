7e150b2f57c2fda92374b729e6808274611b34fb
harviz <harvizh@gmail.com>
2015-09-12 23:26:40 +0300

    20150912_2

--- phpsnipets1.php
+++ phpsnipets1.php
@@ -1,7 +1,6 @@
 <?php 
 
-/**/
-$var = '1';
+/* SWITCH */
 switch ($var)
 {
 	case '1':
@@ -11,6 +10,50 @@ switch ($var)
 	case '3':
 		break;
 }
+
+/* IF */
+if ($a > $b)
+    echo "a is bigger than b";
+
+if ($a > $b)
+{   echo "a is bigger than b";
+    $b = $a;}
+
+/* IF ELSE */
+if ($a > $b)
+{   echo "a is greater than b";}
+else
+{   echo "a is NOT greater than b";}
+
+/* IF ELSEIF ELSE */
+if ($a > $b)
+{   echo "a is bigger than b";}
+elseif ($a == $b)
+{   echo "a is equal to b";}
+else
+{   echo "a is smaller than b";}
+
+if($a > $b):
+    echo $a." is greater than ".$b;
+elseif($a == $b): // Note the combination of the words.
+    echo $a." equals ".$b;
+else:
+    echo $a." is neither greater than or equal to ".$b;
+endif;
+
+/* WHILE */
+while ($i <= 10)
+{   echo $i++;  }
+
+while ($i <= 10):
+    echo $i;
+    $i++;
+endwhile;
+
+
+
+
+
 
 
 
